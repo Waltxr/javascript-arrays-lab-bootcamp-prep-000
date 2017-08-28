e5f5791dc6a702f9b1db030c9020a377cc53cf83
Adam Walter <adam.iverson.walter@gmail.com>
2017-08-28 18:51:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name) {
-    kittens.unshift(name)
+    kittens.push(name)
     return kittens
 }
